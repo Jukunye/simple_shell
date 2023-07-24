59a1cac74746ddbd7d4aac82600b8d2ac0ddab6e
LisaPowell1991 <lisapowell46@gmail.com>
2023-07-24 05:29:54 -0700

    added helper.c

---
+++ helper.c
@@ -0,0 +1,26 @@
+#include "_shell.h"
+
+void removeNewline(char *str)
+{
+	int len;
+
+	len = strlen(str);
+
+	if (len > 0 && str[len - 1] == '\n')
+		str[len - 1] = '\0';
+}
+
+void free_array(char **ptr)
+{
+	int i;
+
+	if (ptr != NULL)
+	{
+		for (i = 0; ptr[i]; i++)
+		{
+			free(ptr[i]);
+		}
+		free(ptr);
+		ptr = NULL;
+	}
+}
