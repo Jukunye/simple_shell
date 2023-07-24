9fad23cc8d006304a5dedff868c2d8f0081eddf6
LisaPowell1991 <lisapowell46@gmail.com>
2023-07-24 05:44:02 -0700

    added path.c

---
+++ path.c
@@ -0,0 +1,39 @@
+#include "_shell.h"
+
+char *find_path(char *cmd)
+{
+	char *path, *pathcp, *dir, *temp;
+	struct stat st;
+
+	if (stat(cmd, &st) == 0)
+		return (cmd);
+	pathcp = getenv("PATH");
+	if (pathcp == NULL)
+		return (NULL);
+
+	path = strdup(pathcp);
+
+	temp = strtok(path, ":");
+
+	while (temp != NULL)
+	{
+		dir = malloc(strlen(temp) + strlen(cmd) + 2);
+
+		dir = strcpy(dir, temp);
+		dir = strcat(dir, "/");
+		dir = strcat(dir, cmd);
+
+		if (stat(dir, &st) == 0)
+		{
+			free(path);
+			return (dir);
+		}
+		free(dir);
+
+		temp = strtok(NULL, ":");
+	}
+	free(path);
+
+
+	return (NULL);
+}
